7d342d7dc1ea635e0133f2e849e0d238c0a767ae
Panagiotis Roubatsis <proubatsis@gmail.com>
2019-02-10 12:49:52 -0500

    Verify z component for Vec3

--- src/test/scala/ca/panagiotis/scalavec/Vec3Spec.scala
+++ src/test/scala/ca/panagiotis/scalavec/Vec3Spec.scala
@@ -19,11 +19,13 @@ class Vec3Spec extends FlatSpec with Matchers {
     val expected2 = Vec3(8.6, 6.6, 9.0)
     assert(actual2.x === expected2.x +- spread)
     assert(actual2.y === expected2.y +- spread)
+    assert(actual2.z === expected2.z +- spread)
 
     val actual3 = Vec3(5.2f, 2.1f, 7.75f) + Vec3(3.4f, 4.5f, 1.25f)
     val expected3 = Vec3(8.6f, 6.6f, 9.0f)
     assert(actual3.x === expected3.x +- spreadf)
     assert(actual3.y === expected3.y +- spreadf)
+    assert(actual3.z === expected3.z +- spreadf)
   }
 
   it should "subtract from another Vec3 object" in {
@@ -33,11 +35,13 @@ class Vec3Spec extends FlatSpec with Matchers {
     val expected1 = Vec3(1.8, -2.4, 6.5)
     assert(actual1.x === expected1.x +- spread)
     assert(actual1.y === expected1.y +- spread)
+    assert(actual1.z === expected1.z +- spread)
 
     val actual2 = Vec3(5.2f, 2.1f, 7.75f) - Vec3(3.4f, 4.5f, 1.25f)
     val expected2 = Vec3(1.8f, -2.4f, 6.5f)
     assert(actual2.x === expected2.x +- spreadf)
     assert(actual2.y === expected2.y +- spreadf)
+    assert(actual2.z === expected2.z +- spreadf)
   }
 
   it should "be scalable" in {
@@ -47,21 +51,25 @@ class Vec3Spec extends FlatSpec with Matchers {
     val expected1 = Vec3(15.9, 6.3, 23.25)
     assert(actual1.x === expected1.x +- spread)
     assert(actual1.y === expected1.y +- spread)
+    assert(actual1.z === expected1.z +- spread)
 
     val actual2 = Vec3(5.3, 2.1, 7.75) * 3.0
     val expected2 = Vec3(15.9, 6.3, 23.25)
     assert(actual2.x === expected2.x +- spread)
     assert(actual2.y === expected2.y +- spread)
+    assert(actual2.z === expected2.z +- spread)
 
     val actual3 = Vec3(5.3f, 2.1f, 7.75f) * 3
     val expected3 = Vec3(15.9f, 6.3f, 23.25f)
     assert(actual3.x === expected3.x +- spreadf)
     assert(actual3.y === expected3.y +- spreadf)
+    assert(actual3.z === expected3.z +- spreadf)
 
     val actual4 = Vec3(5.3f, 2.1f, 7.75f) * 3.0f
     val expected4 = Vec3(15.9f, 6.3f, 23.25f)
     assert(actual4.x === expected4.x +- spreadf)
     assert(actual4.y === expected4.y +- spreadf)
+    assert(actual4.z === expected4.z +- spreadf)
   }
 
   it should "calculate the dot product" in {
@@ -81,16 +89,19 @@ class Vec3Spec extends FlatSpec with Matchers {
      val expected1 = Vec3(0.56614, 0.22646, 0.79259)
      assert(actual1.x === expected1.x +- spread)
      assert(actual1.y === expected1.y +- spread)
+     assert(actual1.z === expected1.z +- spread)
 
      val actual2 = Vec3(5.3, 2.1, 7.75).normalize
      val expected2 = Vec3(0.55088, 0.21827, 0.80553)
      assert(actual2.x === expected2.x +- spread)
      assert(actual2.y === expected2.y +- spread)
+     assert(actual2.z === expected2.z +- spread)
 
      val actual3 = Vec3(5.3f, 2.1f, 7.75f).normalize
      val expected3 = Vec3(0.55088, 0.21827, 0.80553)
      assert(actual3.x === expected3.x +- spread)
      assert(actual3.y === expected3.y +- spread)
+     assert(actual3.z === expected3.z +- spread)
   }
 
   it should "be convertible to a Vec3[Double] object" in {
